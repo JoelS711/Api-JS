22cc144ee740e8c4a5d9b7a3e0acdbb17c01de03
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-03-19 22:51:18 -0500

    Se modifican parametros la Api

--- Weather/app/app.js
+++ Weather/app/app.js
@@ -20,32 +20,18 @@ const search = async (city) => {
 
 search();*/
 
-const urlCity =
-  'https://geocoding-by-api-ninjas.p.rapidapi.com/v1/geocoding?city=Seattle';
+const apiKey = '1089dcb39c721ba93dc3887a24ad9903';
+const city = 'bogota';
+const urlApi =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  
-const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '14485be47bmshc38698980650761p152eb5jsna39813b74d23',
-      'X-RapidAPI-Host': 'geocoding-by-api-ninjas.p.rapidapi.com'
-    }
-  };
-
+ 
 
 const search = async (city) => {
   try {
-    const response = await fetch(urlCity, options);
+    const response = await fetch(urlApi);
     const result = await response.json();
-    const lat = result[0].latitude.toFixed(2);
-    const lon = result[0].longitude.toFixed(2);
-    console.log(`lat: ${lat}, lon: ${lon}`);
     console.log(result);
-    const urlApi =
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=1089dcb39c721ba93dc3887a24ad9903`;
-    const resApi = await fetch(urlApi);
-    const resulApi = await resApi.json();
-    console.log(resulApi);
 
 
 
