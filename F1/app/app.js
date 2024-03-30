45e749351a4189e908d4d441a0283bcce3b534e7
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-03-29 19:28:17 -0500

    Se realiza la card left

--- F1/app/app.js
+++ F1/app/app.js
@@ -7,6 +7,21 @@ const requestOptions = {
   redirect: "follow",
 };
 const searchDriver = document.getElementById("searchDriver");
+const driverNumber = document.getElementById("driverNumber");
+const driverLastName = document.getElementById("driverLastName");
+const driverName = document.getElementById("driverName");
+const driverFlag = document.getElementById("driverFlag");
+const codCountry = document.getElementById("codCountry");
+const imgDriver = document.getElementById("imgDriver");
+const driverPoints = document.getElementById("driverPoints");
+const scuderia = document.getElementById("scuderia");
+const imgScuderia = document.getElementById("imgScuderia");
+const background = document.getElementById("background");
+
+function removeAccents(str) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
 
 const search = async (nameDriver) => {
   console.log(nameDriver);
@@ -32,14 +47,45 @@ const search = async (nameDriver) => {
       }
 
       console.log(result[0]);
+      console.log(nameDriver);
+      driverNumber.innerHTML = `${result[0].driver_number}`;
+      driverName.innerHTML = `${result[0].first_name}`;
+      driverLastName.innerHTML = `${result[0].last_name}`;
+      const countryCodeResponse = await fetch('./app/code_country.json');
+      const countryCodeData = await countryCodeResponse.json();
+      const country = result[0].country_code;
+      const getCountryCode = (country) => countryCodeData[country];
+      console.log(getCountryCode(country));
+      driverFlag.src = `https://flagsapi.com/${getCountryCode(country)}/shiny/64.png`;
+      codCountry.innerHTML = `${country}`;
+      imgDriver.src = `assets/Drivers/${result[0].driver_number}.png`
+      imgDriver.alt = `${result[0].last_name}`;
+      console.log(result[0].driver_number);
+      const driverPointsResponse = await fetch('https://ergast.com/api/f1/current/driverStandings.json');
+      const driverPointsData = await driverPointsResponse.json();
+      console.log(driverPointsData);
+      const lastName = `${result[0].last_name}`;
+      const getPoints = (driverPointsData, lastName) => {
+        const pilot = driverPointsData?.MRData?.StandingsTable?.StandingsLists
+          ?.flatMap(list => list.DriverStandings ?? [])
+          .find(pilot => removeAccents(pilot.Driver.familyName) === lastName);
+          //console.log(driverPointsData);
+        return pilot.points
+      }
+      driverPoints.innerHTML = `${getPoints(driverPointsData, lastName)}`;
+      scuderia.innerHTML = `${result[0].team_name}`;
+      imgScuderia.src = `assets/Constructors/${result[0].team_name}.png`
+      imgScuderia.alt = `${result[0].team_name}`;
+      background.style.backgroundColor = `#${result[0].team_colour}`
 
     }
-
   } catch (error) {
     console.error(error);
     alert("Ocurrio un error al buscar el piloto " + error.message);
   }
 };
+
+
 
 searchDriver.addEventListener("blur", (evento) => {
   let nameDriver = evento.target.value.charAt(0).toUpperCase() + evento.target.value.slice(1).toLowerCase();
